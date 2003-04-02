1e61eb2b5f5f9bf68bfae52926814c7eab1eeb2c
Alex Iliev <alex.iliev@gmail.com>
2003-04-02 22:11:31 +0000

    wrapper class hierarchy for sockets.

---
+++ socket-class.cc
@@ -0,0 +1,126 @@
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/sccnet.h>
+#include <netinet/in.h> /* for htons */
+
+#include <common/utils.h>
+#include <common/sccutils.h>
+#include <common/consts.h>
+
+#include "socket-class.h"
+
+
+using namespace std;
+
+#define THROW_COMM_EX(place) throw comm_exception (string (place ": ") + \
+						   strerror (errno))
+
+
+
+
+SCCDatagramSocket::SCCDatagramSocket ()
+    throw (comm_exception)
+    : DatagramSocket ()
+{
+    initsock();
+}
+
+
+SCCDatagramSocket::SCCDatagramSocket (const SCCSocketAddress & local_addr)
+    throw (comm_exception)
+    : DatagramSocket (),
+      _local_addr (local_addr)
+{
+    initsock();
+    bind (_local_addr);
+}
+
+
+
+void SCCDatagramSocket::bind (const SocketAddress & local_addr)
+    throw (comm_exception)
+{
+    _local_addr = static_cast<const SCCSocketAddress&> (local_addr);
+    
+    // name the socket
+    if ( scc_name_server_sock (_sock, _local_addr.port) != 0 ) {
+	THROW_COMM_EX ("SCCDatagramSocket::bind");
+    }
+
+}
+
+
+ByteBuffer SCCDatagramSocket::recv (SocketAddress & o_source)
+    throw (comm_exception)
+{
+    // receive into a fixed buffer and return also the address of the sender
+
+    int rc;
+    struct sockaddr_scc src_addr;
+    socklen_t scclen = sizeof(src_addr);
+    
+    ByteBuffer answer (new byte[BUFSIZE], BUFSIZE);
+
+    if ((rc = recvfrom
+	 (_sock, answer.data(), answer.len(), 0,
+	  reinterpret_cast<struct sockaddr *>(&src_addr), &scclen)) < 0)
+    {
+	THROW_COMM_EX ("SCCDatagramSocket::recv");
+    }
+
+    if (static_cast<unsigned>(rc) == answer.len()) {
+	// maybe we got a truncation!
+	// ...
+    }
+
+    // the upcast here destroys the object!
+    o_source = SCCSocketAddress (src_addr);
+    
+    answer.len() = rc;
+
+    return answer;
+}
+
+
+
+void SCCDatagramSocket::send (const ByteBuffer & data,
+			      const SocketAddress & dest)
+    throw (comm_exception)
+{
+    int rc;
+    struct sockaddr_scc scc_dest;
+    // NOTE: unpredicatble behavior if dest is not a SCCSocketAddress!
+    const SCCSocketAddress & sccdest =
+	static_cast<const SCCSocketAddress &> (dest);
+
+    scc_dest.scc_family = AF_SCC;
+    scc_dest.scc_card =   htons(sccdest.cardno);
+    scc_dest.scc_port =   htons(sccdest.port);
+
+    if ( (rc = sendto (_sock, data.data(), data.len(),
+		       0,
+		       reinterpret_cast<struct sockaddr *> (&scc_dest),
+		       sizeof (scc_dest)))
+	 != static_cast<int>(data.len()) )
+    {
+	THROW_COMM_EX ("SCCDatagramSocket::send");
+    }
+}
+
+
+
+void SCCDatagramSocket::initsock () throw (comm_exception) {
+    // just create the socket
+    _sock = socket (PF_SCC, SOCK_DGRAM, 0);
+
+    if (_sock == -1) {
+	THROW_COMM_EX ("SCCDatagramSocket::initsock");
+    }
+}
+
+
+SCCDatagramSocket::~SCCDatagramSocket () {
+    if ( close (_sock) != 0 ) {
+	// ??
+    }
+}
---
+++ socket-class.h
@@ -0,0 +1,101 @@
+/* -*- c++ -*-
+ */
+
+#include <common/exceptions.h>
+
+// abstract class
+class SocketAddress {
+};
+
+
+class DatagramSocket {
+public:
+    
+    virtual void bind (const SocketAddress & local_addr)
+	throw (comm_exception) = 0;
+    
+    virtual void send (const ByteBuffer & data, const SocketAddress & dest)
+	throw (comm_exception) = 0;
+
+    virtual ByteBuffer recv (SocketAddress & o_source)
+	throw (comm_exception) = 0;
+
+
+    virtual ~DatagramSocket () {}
+};
+
+
+
+//
+// some concrete guys
+//
+
+//
+// SCC sockets
+//
+
+
+class SCCSocketAddress : public SocketAddress {
+
+public:
+    SCCSocketAddress (unsigned short port)
+	: cardno (SCCADDR_ANY),
+	  port (port)
+	{}
+    
+    SCCSocketAddress (unsigned short cardno, unsigned short port)
+	: cardno(cardno),
+	  port(port)
+	{}
+
+    SCCSocketAddress (const struct sockaddr_scc& scc_addr)
+	: cardno (scc_addr.scc_card),
+	  port   (scc_addr.scc_port)
+	{}
+
+    SCCSocketAddress () {}
+
+    
+    unsigned short cardno, port;
+};
+
+
+
+
+//
+// this class should as one can imagine be given SCCSocketAddress's in the
+// overriden functions
+//
+class SCCDatagramSocket : public DatagramSocket {
+
+public:
+    
+    // create an unbound socket, presumably for sending
+    SCCDatagramSocket () throw (comm_exception);
+    
+    // create a bound socket
+    explicit SCCDatagramSocket (const SCCSocketAddress & local_addr)
+	throw (comm_exception);
+
+
+    
+    virtual void bind (const SocketAddress & local_addr)
+	throw (comm_exception);
+    
+    virtual void send (const ByteBuffer & data, const SocketAddress & dest)
+	throw (comm_exception);
+
+    virtual ByteBuffer recv (SocketAddress & o_source)
+	throw (comm_exception);
+
+
+    virtual ~SCCDatagramSocket ();
+
+
+private:
+
+    void initsock () throw (comm_exception);
+
+    int _sock;
+    SCCSocketAddress _local_addr;
+};
