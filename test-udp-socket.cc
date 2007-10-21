0182bdd0f945de3bb863ca53754e4be1c866765e
Alex Iliev <alex.iliev@gmail.com>
2007-10-21 22:12:17 +0000

    Reduced the license text in the code files, and added a LICENSE.txt file with the full BSD license.

--- test-udp-socket.cc
+++ test-udp-socket.cc
@@ -1,3 +1,19 @@
+/*
+ * Library routines for the Dartmouth CS Private Information Retrieval and
+ * Writing (PIR/W) library.
+ *
+ * Copyright (C) 2003-2007, Alexander Iliev <sasho@cs.dartmouth.edu>
+ *
+ * All rights reserved.
+ *
+ * This code is released under a BSD license.
+ * Please see LICENSE.txt for the full license and disclaimers.
+ *
+ * This product includes cryptographic software written by Eric Young
+ * (eay@cryptsoft.com)
+*/
+
+
 #include <string>
 
 #include <sys/socket.h>
