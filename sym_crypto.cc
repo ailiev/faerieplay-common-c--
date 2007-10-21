0984c7616d3bb66f7759f60f9bccb645a2ea5493
Alex Iliev <alex.iliev@gmail.com>
2007-10-21 23:58:46 +0000

    Added Sean to copyright.

--- sym_crypto.cc
+++ sym_crypto.cc
@@ -2,7 +2,8 @@
  * Library routines for the Dartmouth CS Private Information Retrieval and
  * Writing (PIR/W) library.
  *
- * Copyright (C) 2003-2007, Alexander Iliev <sasho@cs.dartmouth.edu>
+ * Copyright (C) 2003-2007, Alexander Iliev <sasho@cs.dartmouth.edu> and
+ * Sean W. Smith <sws@cs.dartmouth.edu>
  *
  * All rights reserved.
  *
